192944fbb5bf39b0098aea3ddd7bf1131470888f
Michail Chatzimanolakis <michaich@eu-login-30.euler.ethz.ch>
2020-04-28 13:17:43 +0200

    Optimal sensor placement: updated get_data.py

--- applications/osp/get_data.py
+++ applications/osp/get_data.py
@@ -4,7 +4,11 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
-from epidemics.cantons.py.model import  get_canton_reference_data
+import epidemics.data.swiss_cantons as swiss_cantons
+
+def get_canton_reference_data():
+    cases_per_country = swiss_cantons.fetch_openzh_covid_data()
+    return cases_per_country
 
 CANTON_TO_INDEX = {'AG': 0, 'AI': 1, 'AR': 2, 'BE': 3, 'BL': 4, 'BS': 5, 'FR': 6, 'GE': 7, 'GL': 8, 'GR': 9, 'JU': 10, 'LU': 11, 'NE': 12, 'NW': 13, 'OW': 14, 'SG': 15, 'SH': 16, 'SO': 17, 'SZ': 18, 'TG': 19, 'TI': 20, 'UR': 21, 'VD': 22, 'VS': 23, 'ZG': 24, 'ZH': 25}
 
@@ -12,10 +16,9 @@ name = ['AG','AI','AR','BE','BL','BS','FR','GE','GL','GR',\
         'JU','LU','NE','NW','OW','SG','SH','SO','SZ','TG',\
         'TI','UR','VD','VS','ZG','ZH']
 
-IR = get_canton_reference_data().cases_per_country
-
 days = len(IR['TI'])
 cantons = 26
+IR = get_canton_reference_data()
 
 all_data = np.zeros((cantons,days))
 for c in range(cantons):
