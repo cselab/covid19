0f5324699e81a9f1708d4f8a99e7d9f98b13d885
Martin <mboden@barry.ethz.ch>
2020-04-28 10:40:59 +0200

    update plotting script

--- applications/hierarchical/plot_hierarchical.py
+++ applications/hierarchical/plot_hierarchical.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../../')
-from epidemics.data.files.canton_population import CANTON_LIST
+from epidemics.data.files.canton_population import CANTON_LIST, CANTON_LIST_SHORT
 import os
 from subprocess import call
 import argparse
@@ -17,12 +17,12 @@ def create_shell_script(shell_filename):
     with open(shell_filename, 'a') as file:
         file.write('#!/bin/sh \n')
         
-def plot_phase_1(model):
+def plot_phase_1(model,regions):
 
     shell_filename = 'plot_phase_1.sh'
     create_shell_script(shell_filename)
 
-    for canton in CANTON_LIST:
+    for canton in regions:
         phase_1_dir = './data/'+str(model)+'/phase_1_results/'
         samples_dir = phase_1_dir+str(canton)+'/'+str(model)+'/_korali_samples/'
         output_file = phase_1_dir+str(canton)+'/'+str(model)+'/figures/posterior.png'
@@ -54,14 +54,14 @@ def plot_phase_2(model):
     rx = call('./'+shell_filename)
     os.remove(shell_filename)
 
-def plot_phase_3(model):
+def plot_phase_3(model,regions):
 
     shell_filename = 'plot_phase_3.sh'
     create_shell_script(shell_filename)
 
-    for canton in CANTON_LIST:
+    for canton in regions:
         phase_3_dir = './data/'+str(model)+'/phase_3_results/'
-        samples_dir = phase_3_dir+str(canton)+'/'+str(model)+'/_korali_samples/'
+        samples_dir = phase_3_dir+str(canton)+'/'+str(model)+'/_korali_samples_phase_3/'
         output_file = phase_3_dir+str(canton)+'/'+str(model)+'/figures/posterior.png'
         create_folder(phase_3_dir+str(canton)+'/'+str(model)+'/figures')
 
@@ -73,24 +73,48 @@ def plot_phase_3(model):
     rx = call('./'+shell_filename)
     os.remove(shell_filename)
 
+def plot_move_files(model,regions):
+
+    for canton in regions:
+        out_dir = './data/'+str(model)+'/figures/'+canton
+        create_folder(out_dir)
+
+        phase_1_dir = './data/'+str(model)+'/phase_1_results/'+canton+'/'+model+'/figures/'
+
+        call(['cp', phase_1_dir+'prediction.png', out_dir+'/prediction_phase_1.png'])
+        call(['cp', phase_1_dir+'posterior.png', out_dir+'/posterior_phase_1.png'])
+
+        phase_3_dir = './data/'+str(model)+'/phase_3_results/'+canton+'/'+model
+
+        call(['cp', phase_3_dir+'/figures/posterior.png', out_dir+'/posterior_phase_3.png'])
+        call(['cp', phase_3_dir+'/'+canton+'/'+model+'/figures/prediction.png', out_dir+'/prediction_phase_3.png'])
+
 if __name__ == "__main__":  
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--phases', '-ph', default='all', help='Which phases to plor.')
     parser.add_argument('--model', '-m', default='sir_altone_nbin', help='Model type')
+    parser.add_argument('--regions', '-r', default='cantons_short', help='Model type')
+
     args = parser.parse_args()
 
+    if args.regions == 'cantons':
+        regions = CANTON_LIST
+    elif args.regions == 'cantons_short':
+        regions = CANTON_LIST_SHORT
+
     if args.phases == 'all':
-        plot_phase_1(args.model)
+        plot_phase_1(args.model,regions)
         plot_phase_2(args.model) 
-        plot_phase_3(args.model)  
+        plot_phase_3(args.model,regions)  
     elif args.phases == '1':
-        plot_phase_1(args.model)
+        plot_phase_1(args.model,regions)
     elif args.phases == '2':
         plot_phase_2(args.model)
     elif args.phases == '3':
-        plot_phase_3(args.model)
-
+        plot_phase_3(args.model,regions)
+    elif args.phases == 'move':
+        plot_move_files(args.model,regions)
 
 
 
