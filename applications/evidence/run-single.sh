9ca7cef28061d5004e0d17b03e4dd2a4e0f0391e
wadaniel <wadaniel@ethz.ch>
2020-05-22 10:00:08 +0200

    gui model adjusted

--- applications/evidence/run-single.sh
+++ applications/evidence/run-single.sh
@@ -1,5 +1,7 @@
 #PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_int_r0.tnrm" -c "china" --sampler 'mTMCMC'
-PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_int_r0.tnrm" -c "germany" --sampler 'mTMCMC'
+#PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_int_r0.tnrm" -c "germany" --sampler 'mTMCMC'
 #PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_int_r0.tnrm" -c "russia" --sampler 'mTMCMC'
 #PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_int_r0.tnrm" -c "sweden" --sampler 'mTMCMC'
 #PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_int.tnrm" -c "switzerland" --sampler 'mTMCMC'
+#PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir_gui.tnrm" -c "switzerland" --sampler 'mTMCMC'
+PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py -nt 12 -ns 5000 -cm "country.sir.tnrm" -c "switzerland" --sampler 'mTMCMC'
---
+++ epidemics/country/sir_gui/nbin.py
@@ -0,0 +1,152 @@
+import os
+import sys
+
+import numpy as np
+
+from epidemics.tools.tools import save_file
+from .model_base import ModelBase
+
+class Model( ModelBase ):
+
+
+  def __init__( self, **kwargs ):
+
+    self.modelName        = 'country.sir_gui.nbin'
+    self.modelDescription = 'Fit SIR with Intervention on Daily Infected Data with Negative Binomial likelihood'
+    self.likelihoodModel  = 'Negative Binomial'
+
+    super().__init__( **kwargs )
+
+    self.process_data()
+
+
+  def save_data_path( self ):
+      return ( self.dataFolder, self.country, self.modelName )
+
+
+  def process_data( self ):
+
+    y = self.regionalData.infected
+    t = self.regionalData.time
+    N = self.regionalData.populationSize
+    I0 = y[0]
+    S0 = N - I0
+    y0 = S0, I0
+
+    if self.nValidation == 0:
+      self.data['Model']['x-data'] = t[1:]
+      self.data['Model']['y-data'] = np.diff( y[0:])
+    else:
+      self.data['Model']['x-data'] = t[1:-self.nValidation]
+      self.data['Model']['y-data'] = np.diff( y[0:-self.nValidation] )
+      self.data['Validation']['x-data'] = t[-self.nValidation:]
+      self.data['Validation']['y-data'] = np.diff( y[-self.nValidation-1:] )
+
+    self.data['Model']['Initial Condition'] = y0
+    self.data['Model']['Population Size'] = self.regionalData.populationSize
+
+    T = np.ceil( t[-1] + self.futureDays )
+    self.data['Propagation']['x-data'] = np.linspace(0,T,int(T+1))
+
+    save_file( self.data, self.saveInfo['inference data'], 'Data for Inference', 'pickle' )
+
+
+  def get_variables_and_distributions( self ):
+ 
+    p = ['R0', 'tact', 'kbeta', 'Sigma']
+
+    js = {}
+    js['Variables']=[]
+    js['Distributions']=[]
+
+    for k,x in enumerate(p):
+      js['Variables'].append({})
+      js['Variables'][k]['Name'] = x
+      js['Variables'][k]['Prior Distribution'] = 'Prior for ' + x
+
+    self.nParameters = len(p)
+ 
+    k=0
+    js['Distributions'].append({})
+    js['Distributions'][k]['Name'] = 'Prior for R0'
+    js['Distributions'][k]['Type'] = 'Univariate/Uniform'
+    js['Distributions'][k]['Minimum'] = 1.0
+    js['Distributions'][k]['Maximum'] = 4.0
+
+    k+=1
+    js['Distributions'].append({})
+    js['Distributions'][k]['Name'] = 'Prior for tact'
+    js['Distributions'][k]['Type'] = 'Univariate/Uniform'
+    js['Distributions'][k]['Minimum'] = 0.0
+    js['Distributions'][k]['Maximum'] = 80.
+ 
+    k+=1
+    js['Distributions'].append({})
+    js['Distributions'][k]['Name'] = 'Prior for kbeta'
+    js['Distributions'][k]['Type'] = 'Univariate/Uniform'
+    js['Distributions'][k]['Minimum'] = 0.1
+    js['Distributions'][k]['Maximum'] = 10.0
+
+    k+=1
+    js['Distributions'].append({})
+    js['Distributions'][k]['Name'] = 'Prior for [r]'
+    js['Distributions'][k]['Type'] = 'Univariate/Uniform'
+    js['Distributions'][k]['Minimum'] = 0.
+    js['Distributions'][k]['Maximum'] = 50.
+
+    return js
+
+
+  def computational_model( self, s ):
+    p = s['Parameters']
+    t  = self.data['Model']['x-data']
+    y0 = self.data['Model']['Initial Condition']
+    N  = self.data['Model']['Population Size']
+
+    tt = [t[0]-1] + t.tolist()
+    sol = self.solve_ode(y0=y0,T=t[-1], t_eval = tt,N=N,p=p)
+    y = -np.diff(sol.y[0])
+    
+    # get incidents
+    y = -np.diff(sol.y[0])
+     
+    eps = 1e-32
+    y[y < eps] = eps
+ 
+    if(self.sampler == 'mTMCMC'):
+        print("[Epidemics] mTMCMC not yet available for nbin")
+        sys.exit(0)
+
+    s['Reference Evaluations'] = list(y)
+    s['Dispersion'] = ( p[-1] * y ).tolist()
+
+
+
+  def computational_model_propagate( self, s ):
+    p = s['Parameters']
+    t  = self.data['Propagation']['x-data']
+    y0 = self.data['Model']['Initial Condition']
+    N  = self.data['Model']['Population Size']
+
+    tt = [t[0]-1] + t.tolist()
+    sol = self.solve_ode(y0=y0,T=t[-1],t_eval=t.tolist(), N=N,p=p)
+    
+    y = -np.diff(sol.y[0])
+    y = np.append(0, y)
+
+    eps = 1e-32
+    y[y < eps] = eps
+ 
+    js = {}
+    js['Variables'] = []
+
+    js['Variables'].append({})
+    js['Variables'][0]['Name']   = 'Daily Incidence'
+    js['Variables'][0]['Values'] = list(y)
+
+    js['Number of Variables'] = len(js['Variables'])
+    js['Length of Variables'] = len(t)
+
+    js['Dispersion'] = (len(y)) * [p[-1]]
+
+    s['Saved Results'] = js
