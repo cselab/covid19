25538eea77e422cafc8329ea22c3e6d28f6bd872
Ivica Kicic <kicici@ethz.ch>
2020-05-24 22:38:19 +0200

    AD with runtime-specified number of derivatives

--- src/epidemics/utils/autodiff.h
+++ src/epidemics/utils/autodiff.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cmath>
+#include <vector>
 
 namespace epidemics {
 
@@ -21,17 +22,30 @@ struct ADStaticStorage
 {
     static constexpr size_t N() { return N_; }
 
+protected:
     std::array<T, 1 + N_> v_;
+};
+
+template <typename T>
+struct ADDynamicStorage
+{
+    ADDynamicStorage(std::vector<T> v) : v_(std::move(v)) { }
+
+    size_t N() const noexcept { return v_.size() - 1; }
+
+protected:
+    std::vector<T> v_;
 };
 
 /** First partial derivatives class. */
 template <typename Derived, typename T, typename Storage>
-struct AutoDiffBase : protected Storage
+struct AutoDiffBase : Storage
 {
+    using Storage::Storage;
     using ValueType = T;
 
     explicit operator T() const {
-        return this->v_[0];
+        return val();
     }
 
     /// Return the non-derivative value.
@@ -48,52 +62,112 @@ struct AutoDiffBase : protected Storage
     T *dBegin() noexcept { return this->v_.data() + 1; }
     T *dEnd() noexcept { return this->v_.data() + this->N() + 1; }
 
+    // Unary operators.
+    Derived operator+() const {
+        return derived();
+    }
     Derived operator-() const {
-        Derived result;
-        result.val() = -val();
-        for (size_t i = 0; i < this->N(); ++i)
-            result.d(i) = -d(i);
+        Derived result = derived();
+        result.val() = -result.val();
+        for (size_t i = 0; i < result.N(); ++i)
+            result.d(i) = -result.d(i);
         return result;
     }
 
-    friend Derived operator+(const Derived &a, const Derived &b) {
-        assert(a.N() == b.N());
-        Derived result = a;
-        result.val() += b.val();
-        for (size_t i = 0; i < result.N(); ++i)
-            result.d(i) += b.d(i);
-        return result;
+    // Binary operator +.
+    friend Derived operator+(Derived a, T b) {
+        a.val() += b;
+        return a;
     }
-    friend Derived operator-(const Derived &a, const Derived &b) {
-        assert(a.N() == b.N());
-        Derived result = a;
-        result.val() -= b.val();
-        for (size_t i = 0; i < result.N(); ++i)
-            result.d(i) -= b.d(i);
-        return result;
+    friend Derived operator+(T a, Derived b) {
+        b.val() += a;
+        return b;
     }
-    friend Derived operator*(const Derived &a, const Derived &b) {
+    friend Derived operator+(Derived a, const Derived &b) {
         assert(a.N() == b.N());
-        Derived result;
-        result.val() = a.val() * b.val();
-        for (size_t i = 0; i < result.N(); ++i)
-            result.d(i) = a.val() * b.d(i) + a.d(i) * b.val();
-        return result;
-    }
-    friend Derived operator/(const Derived &a, const Derived &b) {
-        T inv_b0 = inverse(b.v_[0]);
-
-        Derived result;
-        result.val() = a.val() * inv_b0;
-        for (size_t i = 0; i < result.N(); ++i)
-            result.d(i) = (a.d(i) - result.val() * b.d(i)) * inv_b0;
-        return result;
+        a.val() += b.val();
+        for (size_t i = 0; i < a.N(); ++i)
+            a.d(i) += b.d(i);
+        return a;
     }
 
+    // Binary operator -.
+    friend Derived operator-(Derived a, T b) {
+        a.val() -= b;
+        return a;
+    }
+    friend Derived operator-(T a, Derived b) {
+        b.val() = a - b.val();
+        for (size_t i = 0; i < b.N(); ++i)
+            b.d(i) = -b.d(i);
+        return b;
+    }
+    friend Derived operator-(Derived a, const Derived &b) {
+        assert(a.N() == b.N());
+        a.val() -= b.val();
+        for (size_t i = 0; i < a.N(); ++i)
+            a.d(i) -= b.d(i);
+        return a;
+    }
+
+    // Binary operator *.
+    friend Derived operator*(Derived a, T b) {
+        a.val() *= b;
+        for (size_t i = 0; i < a.N(); ++i)
+            a.d(i) *= b;
+        return a;
+    }
+    friend Derived operator*(T a, Derived b) {
+        b.val() *= a;
+        for (size_t i = 0; i < b.N(); ++i)
+            b.d(i) *= a;
+        return b;
+    }
+    friend Derived operator*(Derived a, const Derived &b) {
+        assert(a.N() == b.N());
+        T aval = a.val();
+        a.val() *= b.val();
+        for (size_t i = 0; i < a.N(); ++i)
+            a.d(i) = aval * b.d(i) + a.d(i) * b.val();
+        return a;
+    }
+
+    // Binary operator /.
+    friend Derived operator/(Derived a, T b) {
+        return std::move(a) * inverse(b);
+    }
+    friend Derived operator/(T a, Derived b) {
+        T inv_b0 = inverse(b.val());
+
+        b.val() = a * inv_b0;
+        auto factor = -b.val() * inv_b0;
+        for (size_t i = 0; i < b.N(); ++i)
+            b.d(i) *= factor;
+        return b;
+    }
+    friend Derived operator/(Derived a, const Derived &b) {
+        T inv_b0 = inverse(b.val());
+
+        assert(a.N() == b.N());
+        a.val() *= inv_b0;
+        for (size_t i = 0; i < a.N(); ++i)
+            a.d(i) = (a.d(i) - a.val() * b.d(i)) * inv_b0;
+        return a;
+    }
+
+    // Assignment-modify operators.
+    Derived &operator+=(T b) {
+        val() += b;
+        return derived();
+    }
     Derived &operator+=(const Derived &b) {
         val() += b.val();
         for (size_t i = 0; i < this->N(); ++i)
             d(i) += b.d(i);
+        return derived();
+    }
+    Derived &operator-=(T b) {
+        val() -= b;
         return derived();
     }
     Derived &operator-=(const Derived &b) {
@@ -102,11 +176,20 @@ struct AutoDiffBase : protected Storage
             d(i) -= b.d(i);
         return derived();
     }
+    Derived &operator*=(T b) {
+        val() *= b;
+        for (size_t i = 0; i < this->N(); ++i)
+            d(i) *= b;
+        return derived();
+    }
     Derived &operator*=(const Derived &b) {
         for (size_t i = 0; i < this->N(); ++i)
             d(i) = val() * b.d(i) + d(i) * b.val();
         val() *= b.val();
         return derived();
+    }
+    Derived &operator/=(T b) {
+        return (*this *= inverse(b));
     }
     Derived &operator/=(const Derived &b) {
         /* TODO */
@@ -115,43 +198,60 @@ struct AutoDiffBase : protected Storage
     }
 
     Derived inv() const {
-        Derived result;
-        result.val() = inverse(val());
-        auto inv2 = sqr(result.val());
+        Derived result = derived();
+        result.val() = inverse(result.val());
+        auto minv2 = -sqr(result.val());
         for (size_t i = 0; i < result.N(); ++i)
-            result.d(i) = d(i) * -inv2;
+            result.d(i) *= minv2;
         return result;
     }
 
     /* Too lazy to write other operators now... */
-    friend bool operator<(const Derived &a, const Derived &b) {
-        return a.val() < b.val();
+#define EPIDEMICS_AD_CMD_OPERATOR(OP) \
+    friend bool operator OP(const Derived &a, const Derived &b) { \
+        return a.val() OP b.val(); \
+    } \
+    friend bool operator OP(const Derived &a, T b) { \
+        return a.val() OP b; \
+    } \
+    friend bool operator OP(T a, const Derived &b) { \
+        return a OP b.val(); \
     }
-    friend bool operator<(const Derived &a, const T &b) {
-        return a.val() < b;
-    }
-    friend bool operator>(const Derived &a, const Derived &b) {
-        return a.val() > b.val();
-    }
-    friend bool operator>(const Derived &a, const T &b) {
-        return a.val() > b;
-    }
+    EPIDEMICS_AD_CMD_OPERATOR(<);
+    EPIDEMICS_AD_CMD_OPERATOR(<=);
+    EPIDEMICS_AD_CMD_OPERATOR(>);
+    EPIDEMICS_AD_CMD_OPERATOR(>=);
+#undef EPIDEMICS_AD_CMP_OPERATOR
 
+    // Overloads of global functions.
     friend Derived abs(const Derived &x) {
         return x >= 0 ? x : -x;
     }
+
+protected:
+    struct size_tag { };
 
 private:
     Derived &derived() { return *static_cast<Derived *>(this); }
     const Derived &derived() const { return *static_cast<const Derived *>(this); }
 };
 
+/// Autodifferentiation algebra with a compile-time number of derivatives.
 template <typename T, size_t N_>
 struct AutoDiff : AutoDiffBase<AutoDiff<T, N_>, T, ADStaticStorage<T, N_>>
 {
+private:
+    using Base = AutoDiffBase<AutoDiff<T, N_>, T, ADStaticStorage<T, N_>>;
+    using size_tag = typename Base::size_tag;
+
+public:
     static constexpr size_t kNumVariables = N_;
 
     AutoDiff() : AutoDiff(T{}) { }
+    AutoDiff(size_tag, size_t N) : AutoDiff(T{}) {
+        (void)N;
+        assert(N == N_);
+    }
 
     /* implicit */ AutoDiff(T value) {
         this->val() = value;
@@ -165,11 +265,29 @@ struct AutoDiff : AutoDiffBase<AutoDiff<T, N_>, T, ADStaticStorage<T, N_>>
     }
 };
 
+/// Autodifferentiation algebra with a runtime number of derivatives.
+template <typename T>
+struct DynamicAutoDiff : AutoDiffBase<DynamicAutoDiff<T>, T, ADDynamicStorage<T>>
+{
+private:
+    using Base = AutoDiffBase<DynamicAutoDiff<T>, T, ADDynamicStorage<T>>;
+    using size_tag = typename Base::size_tag;
+
+public:
+    DynamicAutoDiff(size_tag, size_t N) : Base(std::vector<T>(N)) { }
+    DynamicAutoDiff(std::vector<T> v) : Base(std::move(v)) { }
+};
+
 template <typename T, typename ...Args>
 AutoDiff<T, sizeof...(Args)> make_ad(T value, Args ...derivatives) {
-    constexpr size_t N = sizeof...(Args); 
+    constexpr size_t N = sizeof...(Args);
     return AutoDiff<T, N>{
         value, std::array<T, N>{static_cast<T>(derivatives)...}};
+}
+
+template <typename T, typename ...Args>
+DynamicAutoDiff<T> make_dynamic_ad(T value, Args ...derivatives) {
+    return DynamicAutoDiff<T>(std::vector<T>{value, static_cast<T>(derivatives)...});
 }
 
 }  // namespace epidemics
