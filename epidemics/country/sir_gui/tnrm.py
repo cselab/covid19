14e115db617097ab1fa393d967f40c2e2e8edde3
Petr Karnakov <kpetr@ethz.ch>
2020-05-25 04:55:26 +0200

    sir_gui: format tnrm.py

--- epidemics/country/sir_gui/tnrm.py
+++ epidemics/country/sir_gui/tnrm.py
@@ -3,87 +3,84 @@ import numpy as np
 from .model_base import ModelBase
 
 
-class Model( ModelBase ):
+class Model(ModelBase):
+    def __init__(self, **kwargs):
 
+        self.modelName = 'country.sir_gui.tnrm'
+        self.modelDescription = 'Fit SIR with interventions on Daily Infected Data with Positive Normal Likelihood'
+        self.likelihoodModel = 'Positive Normal'
 
-  def __init__( self, **kwargs ):
+        super().__init__(**kwargs)
 
-    self.modelName        = 'country.sir_gui.tnrm'
-    self.modelDescription = 'Fit SIR with interventions on Daily Infected Data with Positive Normal Likelihood'
-    self.likelihoodModel  = 'Positive Normal'
+    def get_variables_and_distributions(self):
 
-    super().__init__( **kwargs )
-
-  def get_variables_and_distributions( self ):
- 
-    self.nParameters = 4
-    js = self.get_uniform_priors(
-            ('R0', 1, 4), 
-            ('tact', 0, 80), 
+        self.nParameters = 4
+        js = self.get_uniform_priors(
+            ('R0', 1, 4),
+            ('tact', 0, 80),
             ('kbeta', 0.1, 10),
             ('[Sigma]', 0.01, 100),
-            )
-    
-    return js
+        )
 
-  def computational_model( self, s ):
-    p = s['Parameters']
-    t  = self.data['Model']['x-data']
-    y0 = self.data['Model']['Initial Condition']
-    N  = self.data['Model']['Population Size']
+        return js
 
-    tt = [t[0]-1] + t.tolist()
-    sol = self.solve_ode(y0=y0,T=t[-1], t_eval = tt,N=N,p=p)
+    def computational_model(self, s):
+        p = s['Parameters']
+        t = self.data['Model']['x-data']
+        y0 = self.data['Model']['Initial Condition']
+        N = self.data['Model']['Population Size']
 
-    # get incidents
-    y = -np.diff(sol.y[0])
-     
-    eps = 1e-32
-    y[y < eps] = eps
-    
-    # Transform gradients
-    if(self.sampler == 'mTMCMC'):
-        sgrad    = []
-        diffgrad = []
-        for idx in range(len(y)):
-            tmp = -(sol.gradMu[idx+1]-sol.gradMu[idx])
-            diffgrad.append(list(tmp))
-            
-            tmp = tmp*p[-1]
-            tmp[-1] = y[idx]*sol.gradSig[idx][-1]
-            sgrad.append(list(tmp))
+        tt = [t[0] - 1] + t.tolist()
+        sol = self.solve_ode(y0=y0, T=t[-1], t_eval=tt, N=N, p=p)
 
-        s['Gradient Mean'] = diffgrad
-        s['Gradient Standard Deviation'] = sgrad
+        # get incidents
+        y = -np.diff(sol.y[0])
 
-    s['Reference Evaluations'] = list(y)
-    s['Standard Deviation'] = ( p[-1] * y ).tolist()
+        eps = 1e-32
+        y[y < eps] = eps
 
+        # Transform gradients
+        if (self.sampler == 'mTMCMC'):
+            sgrad = []
+            diffgrad = []
+            for idx in range(len(y)):
+                tmp = -(sol.gradMu[idx + 1] - sol.gradMu[idx])
+                diffgrad.append(list(tmp))
 
-  def computational_model_propagate( self, s ):
-    p = s['Parameters']
-    t  = self.data['Propagation']['x-data']
-    y0 = self.data['Model']['Initial Condition']
-    N  = self.data['Model']['Population Size']
+                tmp = tmp * p[-1]
+                tmp[-1] = y[idx] * sol.gradSig[idx][-1]
+                sgrad.append(list(tmp))
 
-    tt = [t[0]-1] + t.tolist()
-    sol = self.solve_ode(y0=y0,T=t[-1],t_eval=t.tolist(), N=N,p=p)
-    
-    y = -np.diff(sol.y[0])
-    y = np.append(0, y)
+            s['Gradient Mean'] = diffgrad
+            s['Gradient Standard Deviation'] = sgrad
 
-    eps = 1e-32
-    y[y < eps] = eps
-    
-    js = {}
-    js['Variables'] = []
+        s['Reference Evaluations'] = list(y)
+        s['Standard Deviation'] = (p[-1] * y).tolist()
 
-    js['Variables'].append({})
-    js['Variables'][0]['Name']   = 'Daily Incidence'
-    js['Variables'][0]['Values'] = list(y)
+    def computational_model_propagate(self, s):
+        p = s['Parameters']
+        t = self.data['Propagation']['x-data']
+        y0 = self.data['Model']['Initial Condition']
+        N = self.data['Model']['Population Size']
 
-    js['Number of Variables'] = len(js['Variables'])
-    js['Length of Variables'] = len(t)
+        tt = [t[0] - 1] + t.tolist()
+        sol = self.solve_ode(y0=y0, T=t[-1], t_eval=t.tolist(), N=N, p=p)
 
-    js['Standard Deviation'] = ( p[-1] * y ).tolist()
-    s['Saved Results'] = js
+        y = -np.diff(sol.y[0])
+        y = np.append(0, y)
+
+        eps = 1e-32
+        y[y < eps] = eps
+
+        js = {}
+        js['Variables'] = []
+
+        js['Variables'].append({})
+        js['Variables'][0]['Name'] = 'Daily Incidence'
+        js['Variables'][0]['Values'] = list(y)
+
+        js['Number of Variables'] = len(js['Variables'])
+        js['Length of Variables'] = len(t)
+
+        js['Standard Deviation'] = (p[-1] * y).tolist()
+        s['Saved Results'] = js
