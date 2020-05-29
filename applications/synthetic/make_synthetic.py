8cba53d9d50553003df0a0f479d88d355c0faab1
wadaniel <wadaniel@ethz.ch>
2020-05-29 15:29:16 +0200

    added synthetic

--- applications/synthetic/make_synthetic.py
+++ applications/synthetic/make_synthetic.py
@@ -6,6 +6,28 @@ import matplotlib.pyplot as plt
 
 import libepidemics #cpp backend
 
+def sir_beta(y0, t_eval, N, p ):
+    
+    sir       = libepidemics.country.sir
+    data      = libepidemics.country.ModelData(N=N)
+    cppsolver = sir.Solver(data)
+
+    params = sir.Parameters(beta=p[0], gamma=p[1])
+    
+    s0, i0 = y0
+    y0cpp   = (s0, i0, 0.0)
+    initial = sir.State(y0cpp)
+    
+    cpp_res = cppsolver.solve(params, initial, t_eval=t_eval, dt = 0.01)
+    
+    Svec = np.zeros(len(cpp_res))
+
+    for idx,entry in enumerate(cpp_res):
+        Svec[idx] = entry.S()
+
+    # get newly infected
+    return Svec
+
 
 def sir_int_r0(y0, t_eval, N, p ):
     
@@ -19,12 +41,12 @@ def sir_int_r0(y0, t_eval, N, p ):
     y0cpp   = (s0, i0, 0.0)
     initial = sir_int_r0.State(y0cpp)
     
-    cpp_res = cppsolver.solve_params_ad(params, initial, t_eval=t_eval, dt = 0.01)
+    cpp_res = cppsolver.solve(params, initial, t_eval=t_eval, dt = 0.01)
     
     Svec = np.zeros(len(cpp_res))
 
     for idx,entry in enumerate(cpp_res):
-        Svec[idx] = entry.S().val()
+        Svec[idx] = entry.S()
 
     # get newly infected
     return Svec
@@ -82,8 +104,16 @@ if __name__ == "__main__":
     dtact = 10
     kbeta = 0.5
 
-    p = [r0, gamma, tact, dtact, kbeta]
+    beta = gamma*r0
+    psir = [beta, gamma]
 
+    sirS     = sir_beta((S0, I0), teval, N, psir)
+    sirCases = -np.diff(sirS)
+    sirInfected = np.cumsum(sirCases)
+    makefile("sir_raw.txt", "Synthetic SIR Raw", N, sirInfected)
+    
+    p = [r0, gamma, tact, dtact, kbeta]
+    
     S        = sir_int_r0((S0, I0), teval, N, p)
     cases    = -np.diff(S)
     infected = np.cumsum(cases)
--- applications/synthetic/run.sh
+++ applications/synthetic/run.sh
@@ -1,18 +1,15 @@
  #!/bin/bash
 
 declare -a arr=(
-"sir_int_r0_rndm.txt"
+"sir_raw.txt"
 )
-
-#"sir_int_r0_raw.txt"
-
 
 base="./data/"
 
 for f in "${arr[@]}"
 do
-   PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py --silentPlot -nt 12 -ns 5000 -np 5000 -cm "country.sir_int_r0.tnrm" -c "$c" --sampler 'mTMCMC' -df $base --synthetic -dat $f
+   PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py --silentPlot -nt 12 -ns 5000 -np 5000 -cm "country.sir.tnrm" -c "$c" --sampler 'mTMCMC' -df $base --synthetic -dat $f
    
-   folder="$base/country.sir_int_r0.tnrm/"
+   folder="$base/country.sir.tnrm/"
    python3 -m korali.plotter --dir "$folder/_korali_samples"  --output "$folder/figures/samples.png"
 done
