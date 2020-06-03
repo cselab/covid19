71c52b0cc221a62b53581759f37d19782097e265
wadaniel <wadaniel@ethz.ch>
2020-06-03 10:07:59 +0200

    redirect output

--- applications/synthetic/run-sampling-int.sh
+++ applications/synthetic/run-sampling-int.sh
@@ -7,12 +7,12 @@ declare -a arr=(
 
 
 
-model="country.sir_int_r0_nogamma.tnrm"
+model="country.sir_int_r0.tnrm"
 
 for f in "${arr[@]}"
 do
    base="./data/$f/"
-   PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py --silentPlot -nt 12 -ns 10000 -np 10000 -cm $model -c "$c" --sampler 'mTMCMC' -df $base --synthetic -dat "$f.txt"
+   PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py --silentPlot -nt 12 -ns 25000 -np 25000 -cm $model -c "$c" --sampler 'mTMCMC' -df $base --synthetic -dat "$f.txt" | tee out-rsi.txt
    
    folder="$base/$model"
    python3 -m korali.plotter --dir "$folder/_korali_samples"  --output "$folder/figures/samples.png"
