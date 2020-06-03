731e6faaea3ab87ffcb43349f2068ac17731cf84
wadaniel <wadaniel@ethz.ch>
2020-06-03 10:11:19 +0200

    redirect outpur

--- applications/synthetic/run-sampling-ic.sh
+++ applications/synthetic/run-sampling-ic.sh
@@ -12,7 +12,7 @@ model="country.sir_int_r0_IC.tnrm"
 for f in "${arr[@]}"
 do
    base="./data/$f/"
-   PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py --silentPlot -nt 12 -ns 1000 -np 1000 -cm $model -c "$c" --sampler 'mTMCMC' -df $base --synthetic -dat "$f.txt"
+   PYTHONPATH=../..:../../build:$PYTHONPATH python sample.py --silentPlot -nt 12 -ns 1000 -np 1000 -cm $model -c "$c" --sampler 'mTMCMC' -df $base --synthetic -dat "$f.txt" | tee out-rsic.txt
    
    folder="$base/$model"
    python3 -m korali.plotter --dir "$folder/_korali_samples"  --output "$folder/figures/samples.png"
