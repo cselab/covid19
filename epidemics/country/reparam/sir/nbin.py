047511fb5a0028ea69353dfcb8ad71c4b74ec706
Martin <mboden@barry.ethz.ch>
2020-06-12 11:38:31 +0200

    refactor reparametrized models, got rid of some code duplication

--- epidemics/country/reparam/sir/nbin.py
+++ epidemics/country/reparam/sir/nbin.py
@@ -1,11 +1,8 @@
-import sys
-
 import numpy as np
 
 from .model_base import ModelBase
 
 class Model( ModelBase ):
-
 
   def __init__( self, **kwargs ):
 
@@ -25,56 +22,3 @@ class Model( ModelBase ):
         ('[r]', 0.01, 10))
     
     return js
-
-  def computational_model( self, s ):
-    p = s['Parameters']
-    t  = self.data['Model']['x-data']
-    y0 = self.data['Model']['Initial Condition']
-    N  = self.data['Model']['Population Size']
-
-    tt = [t[0]-1] + t.tolist()
-    sol = self.solve_ode(y0=y0,T=t[-1], t_eval = tt,N=N,p=p)
-    
-    # get incidents
-    y = np.diff(sol.y)
-     
-    eps = 1e-32
-    y[y < eps] = eps
- 
-    # Transform gradients
-    if(self.sampler == 'mTMCMC'):
-        print("[Epidemics] mTMCMC not yet available for nbin")
-        sys.exit(0)
-
-    s['Reference Evaluations'] = list(y)
-    s['Dispersion'] = ( p[-1] * y ).tolist()
-
-
-  def computational_model_propagate( self, s ):
-    p = s['Parameters']
-    t  = self.data['Propagation']['x-data']
-    y0 = self.data['Model']['Initial Condition']
-    N  = self.data['Model']['Population Size']
-
-    tt = [t[0]-1] + t.tolist()
-    sol = self.solve_ode(y0=y0,T=t[-1],t_eval=t.tolist(), N=N,p=p)
-    
-    y = np.diff(sol.y)
-    y = np.append(0, y)
-
-    eps = 1e-32
-    y[y < eps] = eps
- 
-    js = {}
-    js['Variables'] = []
-
-    js['Variables'].append({})
-    js['Variables'][0]['Name']   = 'Daily Incidence'
-    js['Variables'][0]['Values'] = list(y)
-
-    js['Number of Variables'] = len(js['Variables'])
-    js['Length of Variables'] = len(t)
-
-    js['Dispersion'] = (len(y)) * [p[-1]]
-
-    s['Saved Results'] = js
--- epidemics/country/reparam/sir/tnrm.py
+++ epidemics/country/reparam/sir/tnrm.py
@@ -2,9 +2,7 @@ import numpy as np
 
 from .model_base import ModelBase
 
-
 class Model( ModelBase ):
-
 
   def __init__( self, **kwargs ):
 
@@ -25,65 +23,3 @@ class Model( ModelBase ):
             )
     
     return js
-
-  def computational_model( self, s ):
-    p = s['Parameters']
-    t  = self.data['Model']['x-data']
-    y0 = self.data['Model']['Initial Condition']
-    N  = self.data['Model']['Population Size']
-
-    tt = [t[0]-1] + t.tolist()
-    sol = self.solve_ode(y0=y0,T=t[-1], t_eval = tt,N=N,p=p)
-
-    # get incidents
-    y = np.diff(sol.y)
-     
-    eps = 1e-32
-    y[y < eps] = eps
-    
-    # Transform gradients
-    if(self.sampler == 'mTMCMC'):
-        sgrad    = []
-        diffgrad = []
-        for idx in range(len(y)):
-            tmp = -(sol.gradMu[idx+1]-sol.gradMu[idx])
-            diffgrad.append(list(tmp))
-            
-            tmp = tmp*p[-1]
-            tmp[-1] = y[idx]*sol.gradSig[idx][-1]
-            sgrad.append(list(tmp))
-
-        s['Gradient Mean'] = diffgrad
-        s['Gradient Standard Deviation'] = sgrad
-
-    s['Reference Evaluations'] = list(y)
-    s['Standard Deviation'] = ( p[-1] * y ).tolist()
-
-
-  def computational_model_propagate( self, s ):
-    p = s['Parameters']
-    t  = self.data['Propagation']['x-data']
-    y0 = self.data['Model']['Initial Condition']
-    N  = self.data['Model']['Population Size']
-
-    tt = [t[0]-1] + t.tolist()
-    sol = self.solve_ode(y0=y0,T=t[-1],t_eval=t.tolist(), N=N,p=p)
-    
-    y = np.diff(sol.y)
-    y = np.append(0, y)
-
-    eps = 1e-32
-    y[y < eps] = eps
-    
-    js = {}
-    js['Variables'] = []
-
-    js['Variables'].append({})
-    js['Variables'][0]['Name']   = 'Daily Incidence'
-    js['Variables'][0]['Values'] = list(y)
-
-    js['Number of Variables'] = len(js['Variables'])
-    js['Length of Variables'] = len(t)
-
-    js['Standard Deviation'] = ( p[-1] * y ).tolist()
-    s['Saved Results'] = js
