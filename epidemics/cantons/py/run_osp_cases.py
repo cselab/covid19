669a7cc7ced4b91e2a4cfd72e10cc66e6e878098
Michail Chatzimanolakis <michaich@eu-login-43.euler.ethz.ch>
2020-06-25 16:09:07 +0200

    OSP: integration over correlation time added

--- epidemics/cantons/py/run_osp_cases.py
+++ epidemics/cantons/py/run_osp_cases.py
@@ -16,13 +16,14 @@ from epidemics.cantons.py.model import \
 import libepidemics
 
 data = get_canton_model_data(include_foreign=False)
+#N0  = list(data.region_population)
+#np.save("population.npy",N0)
 
 
-def example_run_seiin(num_days, inputs):
+def example_run_seiin(num_days, inputs, int_day=21):
     num_days = int(num_days)
 
     cantons_ = [0,3,4,5,6,7,9,15,20,22,23,25]
-
 
     L = len(inputs)
     ic_cantons = len(cantons_)
@@ -33,8 +34,7 @@ def example_run_seiin(num_days, inputs):
     IU0 = [0] * data.num_regions
     IR0[data.key_to_index['TI']] = 1  # Ticino.
 
-    if L == 6 + ic_cantons: #case 1 
-
+    if L == 6 + ic_cantons: #inference/model evaluations for case 2 
        params = libepidemics.cantons.seiin_interventions.Parameters(
                  beta  =inputs[0],
                  mu    =inputs[1],
@@ -42,81 +42,30 @@ def example_run_seiin(num_days, inputs):
                  Z     =inputs[3],
                  D     =inputs[4],
                  theta =inputs[5],
-                 b1    =inputs[0],
-                 b2    =inputs[0],
+
+                 b1    =np.random.uniform(0.0,inputs[0],1),
+                 b2    =np.random.uniform(0.0,inputs[0],1),
                  b3    =inputs[0],
-                 d1    =num_days+1,
+                 d1    =int_day,
                  d2    =num_days+1,
                  d3    =num_days+1,
-                 theta1=inputs[5],
+                 theta1=np.random.uniform(0.0,inputs[5],1),
                  theta2=inputs[5],
                  theta3=inputs[5])
-
        k = 0
        for c in cantons_:
-          IU0[c] =   inputs[6+k]
-          E0 [c] = 3*inputs[6+k]
+          IU0[c] = inputs[6+k]
+          E0 [c] = 3*IU0[c]
           k += 1
-
        S0 = [N - E - IR - IU for N, E, IR, IU in zip(N0, E0, IR0, IU0)]
        y0 = S0 + E0 + IR0 + IU0 + N0
-
        # Run the ODE solver.
        solver = libepidemics.cantons.seiin_interventions.Solver(data.to_cpp())
        y0 = libepidemics.cantons.seiin_interventions.State(y0)
        return solver.solve(params, y0, t_eval=range(1, num_days + 1))
 
 
-    if L == 8 + ic_cantons: #case 2 
-
-       params = libepidemics.cantons.seiin_interventions.Parameters(
-                 beta  =inputs[0],
-                 mu    =inputs[1],
-                 alpha =inputs[2],
-                 Z     =inputs[3],
-                 D     =inputs[4],
-                 theta =inputs[5],
-                 b1    =inputs[6],
-                 b2    =inputs[0],
-                 b3    =inputs[0],
-                 d1    =21,
-                 d2    =num_days+1,
-                 d3    =num_days+1,
-                 theta1=inputs[7],
-                 theta2=inputs[5],
-                 theta3=inputs[5])
-
-       k = 0
-       for c in cantons_:
-          IU0[c] =   inputs[8+k]
-          E0 [c] = 3*inputs[8+k]
-          k += 1
-
-       S0 = [N - E - IR - IU for N, E, IR, IU in zip(N0, E0, IR0, IU0)]
-       y0 = S0 + E0 + IR0 + IU0 + N0
-
-       # Run the ODE solver.
-       solver = libepidemics.cantons.seiin_interventions.Solver(data.to_cpp())
-       y0 = libepidemics.cantons.seiin_interventions.State(y0)
-       return solver.solve(params, y0, t_eval=range(1, num_days + 1))
-
-
-    if L == 12 + ic_cantons: #case 3
-       '''
-       print("Running case 3 with:")
-       print("beta  =",inputs[ 0])
-       print("mu    =",inputs[ 1])
-       print("alpha =",inputs[ 2])
-       print("Z     =",inputs[ 3])
-       print("D     =",inputs[ 4])
-       print("theta =",inputs[ 5])
-       print("b1    =",inputs[ 6])
-       print("b2    =",inputs[ 7])
-       print("d1    =",inputs[ 8])
-       print("d2    =",inputs[ 9])
-       print("theta1=",inputs[10])
-       print("theta2=",inputs[11])
-       '''
+    if L == 12 + ic_cantons : #inference/model evaluations for case 3 
 
        params = libepidemics.cantons.seiin_interventions.Parameters(
                  beta  =inputs[0],
@@ -127,56 +76,31 @@ def example_run_seiin(num_days, inputs):
                  theta =inputs[5],
                  b1    =inputs[6],
                  b2    =inputs[7],
-                 b3    =inputs[0],
+                 b3    =np.random.uniform(0.0,0.5,1),
                  d1    =inputs[8],
                  d2    =inputs[9],
-                 d3    =num_days+1,
+                 d3    =84,
                  theta1=inputs[10],
                  theta2=inputs[11],
                  theta3=inputs[5])
-
        k = 0
        for c in cantons_:
-          IU0[c] =   inputs[12+k]
+          IU0[c] = inputs[12+k]
           E0 [c] = 3*inputs[12+k]
           k += 1
-
        S0 = [N - E - IR - IU for N, E, IR, IU in zip(N0, E0, IR0, IU0)]
        y0 = S0 + E0 + IR0 + IU0 + N0
-
        # Run the ODE solver.
        solver = libepidemics.cantons.seiin_interventions.Solver(data.to_cpp())
        y0 = libepidemics.cantons.seiin_interventions.State(y0)
        return solver.solve(params, y0, t_eval=range(1, num_days + 1))
 
+'''
+p = [1.0,0.5,0.5,3.0,3.0,1.0,10,10,10,10,10,10,10,10,10,10,10,10]
+rr = example_run_seiin(50, p)
+Ir = []
+for d in range(50):
+   Ir.append(rr[d].Ir())
+np.save("output.npy",Ir)
 
-    params = libepidemics.cantons.seiin_interventions.Parameters(
-              beta  =inputs[0],
-              mu    =inputs[1],
-              alpha =inputs[2],
-              Z     =inputs[3],
-              D     =inputs[4],
-              theta =inputs[5],
-              b1    =inputs[6],
-              b2    =inputs[7],
-              b3    =inputs[0],
-              d1    =inputs[8],
-              d2    =inputs[9],
-              d3    =num_days+1,
-              theta1=inputs[10],
-              theta2=inputs[11],
-              theta3=inputs[5])
-
-    k = 0
-    for c in cantons_:
-       IU0[c] =   inputs[12+k]
-       E0 [c] = 3*inputs[12+k]
-       k += 1
-
-    S0 = [N - E - IR - IU for N, E, IR, IU in zip(N0, E0, IR0, IU0)]
-    y0 = S0 + E0 + IR0 + IU0 + N0
-
-    # Run the ODE solver.
-    solver = libepidemics.cantons.seiin_interventions.Solver(data.to_cpp())
-    y0 = libepidemics.cantons.seiin_interventions.State(y0)
-    return solver.solve(params, y0, t_eval=range(1, num_days + 1))
+'''
