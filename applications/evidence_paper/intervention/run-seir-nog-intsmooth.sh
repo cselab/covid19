bbb82302505f10ee4ffc2c9c615f8067d66a0a72
wadaniel <wadaniel@ethz.ch>
2020-08-05 01:26:07 +0200

    added nogamma models

---
+++ applications/evidence_paper/intervention/run-seir-nog-intsmooth.sh
@@ -0,0 +1,50 @@
+ #!/bin/bash
+
+msg="0.5ppm, mu in (0,1)"
+pushd ..
+
+declare -a countries=(
+"switzerland"
+"france"
+"germany"
+"italy"
+"uk"
+"spain"
+"russia"
+"us"
+"canada"
+"australia"
+"china"
+"japan"
+"south korea"
+"turkey"
+)
+
+name=`whoami`
+base="/scratch/${name}/covid19/intervention/data/run2"
+
+declare -a models=(
+"country.reparam.seird_intsmooth_nogamma.nbin"
+#"country.reparam.seird_intsmooth_nogamma.tnrm"
+)
+
+
+mkdir ${base} -p
+
+for model in "${models[@]}"
+do
+    for c in "${countries[@]}"
+    do
+        folder=$base/${c}/$model
+        mkdir -p "${folder}"
+
+        outfile=${folder}/knested.out
+        time PYTHONPATH=../..:../../build:$PYTHONPATH python sample_knested.py \
+            --silentPlot -ns 1500 -dlz 0.1 -cm ${model} -c "${c}" -bs 8 -nt 8 -ui -ud -df $base -m "${msg}" \
+            2>&1 | tee "${outfile}"
+
+        python3 -m korali.plotter --dir "$folder/_korali_samples"  --output "$folder/figures/samples.png"
+        done
+done
+
+popd
---
+++ epidemics/country/reparam/seird_intsmooth_nogamma/model_base.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+
+from epidemics.country.country import EpidemicsCountry
+
+import libepidemics #cpp backend
+
+class Object(object):
+        pass
+
+class ModelBase( EpidemicsCountry ):
+
+
+  def __init__( self, **kwargs ):
+
+    super().__init__( **kwargs )
+
+  def solve_ode( self, y0, T, t_eval, N, p ):
+    
+    seird_int_reparam  = libepidemics.country.seird_intsmooth_reparam
+    dp        = libepidemics.country.DesignParameters(N=N)
+    cppsolver = seird_int_reparam.Solver(dp)
+
+    params = seird_int_reparam.Parameters(R0=p[0], D=1.0/self.constants['gamma'], Z=p[1], eps=p[2], tact=p[3], dtact=p[4], kbeta=p[5])
+    
+    s0, i0  = y0
+    y0cpp   = (s0, 0.0, i0, 0.0, 0.0) # S E I R D
+    initial = seird_int_reparam.State(y0cpp)
+    
+    cpp_res = cppsolver.solve_params_ad(params, initial, t_eval=t_eval, dt = 0.01)
+    
+    infected  = np.zeros(len(cpp_res))
+    recovered = np.zeros(len(cpp_res))
+    exposed   = np.zeros(len(cpp_res))
+    deaths    = np.zeros(len(cpp_res))
+
+    for idx,entry in enumerate(cpp_res):
+        infected[idx]  = N-entry.S().val()-entry.E().val()
+        exposed[idx]   = N-entry.S().val()
+        recovered[idx] = entry.R().val()
+        deaths[idx]    = entry.D().val()
+
+    # Fix bad values
+    infected[np.isnan(infected)] = 0
+    
+    # Create Solution Object
+    sol = Object()
+    sol.y       = infected
+    sol.e       = exposed
+    sol.r       = recovered
+    sol.d       = deaths
+ 
+    return sol
