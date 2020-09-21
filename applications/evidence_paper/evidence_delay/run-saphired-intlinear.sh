1ebc05ac7284b29cc72325d9bb6fedadb9210dd9
wadaniel <wadaniel@ethz.ch>
2020-09-21 17:35:24 +0200

    scripts for multi runs

--- applications/evidence_paper/evidence_delay/run-saphired-intlinear.sh
+++ applications/evidence_paper/evidence_delay/run-saphired-intlinear.sh
@@ -6,31 +6,35 @@ pushd ..
 source countries.sh
 
 name=`whoami`
-base="/scratch/${name}/covid19/data/delay"
 
 declare -a models=(
 "country.reparam.saphiredelay_int.nbin"
 )
 
-mkdir ${base} -p
-
-for model in "${models[@]}"
+for i in {1 .. 5}
 do
-    for c in "${countries[@]}"
+
+    base="/scratch/${name}/covid19/data/delay_${i}"
+
+    for model in "${models[@]}"
     do
-        folder=$base/$c/$model
-        mkdir -p "${folder}"
+        for c in "${countries[@]}"
+        do
+            folder=$base/$c/$model
+            mkdir -p "${folder}"
 
-        outfile=${folder}/knested.out
-        time PYTHONPATH=../..:../../build:$PYTHONPATH python sample_knested.py \
-            --silentPlot -ns 1500 -dlz 0.1 -cm ${model} -c "$c" -ui -ud -uip -bs 8 -nt 8 -df $base -m "${msg}" \
-            2>&1 | tee "${outfile}"
+            outfile=${folder}/knested.out
+            time PYTHONPATH=../..:../../build:$PYTHONPATH python sample_knested.py \
+                --silentPlot -ns 1500 -dlz 0.1 -cm ${model} -c "$c" -ui -ud -uip -bs 8 -nt 8 -df $base -m "${msg}" \
+                2>&1 | tee "${outfile}"
 
-        python3 -m korali.plotter --dir "$folder/_korali_samples"  --output "$folder/figures/samples.png"
-        
-        #rm -r "$folder/_korali_propagation"
-        
-        done
+            #python3 -m korali.plotter --dir "$folder/_korali_samples"  --output "$folder/figures/samples.png"
+            
+            rm -r "$folder/_korali_propagation"
+            
+            done
+    done
+
 done
 
 popd
